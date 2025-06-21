1b82aa1e2098976636168c367e88e3ba0fb144f3
Paolo Mansilla <tokkikun59@gmail.com>
2025-06-21 10:49:22 -0500

    test: verificar impacto al atacar una celda con barco (test_impacto)

---
+++ Casos_De_Testing.py
@@ -0,0 +1,9 @@
+# test_tablero.py
+import pytest
+
+def test_impacto():
+    tablero = Tablero()
+    tablero.colocar_barco(3, 5)
+    resultado = tablero.recibir_ataque(3, 5)
+    assert resultado == "¡Impacto!"
+    assert tablero.celdas[3][5] == 'X'
